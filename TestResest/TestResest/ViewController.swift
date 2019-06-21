d7288b0bc6af12d91b47d8f473a7af161f7c7be9
yumingming <mingmingyush@wecanintl.com>
2019-06-21 10:50:57 +0800

    初始化项目

---
+++ TestResest/TestResest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestResest
+//
+//  Created by erp on 2019/6/21.
+//  Copyright © 2019 erp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestResest/TestResestTests/TestResestTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestResestTests.swift
+//  TestResestTests
+//
+//  Created by erp on 2019/6/21.
+//  Copyright © 2019 erp. All rights reserved.
+//
+
+import XCTest
+@testable import TestResest
+
+class TestResestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
